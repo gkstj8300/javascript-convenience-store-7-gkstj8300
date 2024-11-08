45e048f2ff1c1dd8097ec7c8b9187548fd0e40f5
parkjeehoon <gkstj8300@naver.com>
2024-11-09 01:14:20 +0900

    feat(ErrorMessages): 에러 메시지 상수 정의
    - 사용자 입력 관련 에러 메시지 상수화 하였습니다.
    - 상품 구매 유효성 검사 메시지 추가하였습니다.
    - 파일 로드 에러 메시지 추가하였습니다.

---
+++ src/constants/messages/errorMessages.js
@@ -0,0 +1,12 @@
+const ERROR_MESSAGE = Object.freeze({
+    input_purchase_products_format: '올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
+    input_existing_products: '존재하지 않는 상품입니다. 다시 입력해 주세요.',
+    input_storck_avilability: '재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
+    input_purchase_products_input: '잘못된 입력입니다. 다시 입력해 주세요.',
+    input_not_empty: '입력값이 공백입니다.',
+    input_whether_format: '입력값은 Y혹은 N을 입력하여야 합니다.',
+    input_whether_uppercase: '입력값은 대문자여야 합니다.',
+    load_file_error: '파일을 불러오는데 에러가 발생하였습니다.'
+});
+
+export default ERROR_MESSAGE;
